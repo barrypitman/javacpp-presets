3132009e0eba8894d98a015a5984f3dc6c1bc671
barry <barry@ubiquitech.co.za>
2024-08-26 12:24:05 +0200

    initial mapping

--- hailort/cppbuild.sh
+++ hailort/cppbuild.sh
@@ -11,35 +11,40 @@ HAILORT_VERSION=4.18.0
 download https://github.com/hailo-ai/hailort/archive/refs/tags/v$HAILORT_VERSION.tar.gz hailort-v$HAILORT_VERSION.tar.gz
 mkdir -p $PLATFORM
 cd $PLATFORM
-tar -xzvf ../hailort-$HAILORT_VERSION.tar.gz
+tar -xzvf ../hailort-v$HAILORT_VERSION.tar.gz
 cd hailort-$HAILORT_VERSION
 
 case $PLATFORM in
     android-arm64)
         cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release
         cmake --build build --config release --target libhailort
+        mkdir -p ../include ../lib
         cp ./build/hailort/libhailort/src/libhailort.so.$HAILORT_VERSION ../lib/
         cp hailort/libhailort/include/hailo/*.h ../include/
         ;;
     android-x86_64)
         cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release
         cmake --build build --config release --target libhailort
+        mkdir -p ../include ../lib
         ;;
     linux-x86_64)
         cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release
         cmake --build build --config release --target libhailort
+        mkdir -p ../include ../lib
         cp ./build/hailort/libhailort/src/libhailort.so.$HAILORT_VERSION ../lib/
         cp hailort/libhailort/include/hailo/*.h ../include/
         ;;
     linux-arm64)
         cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release
         cmake --build build --config release --target libhailort
+        mkdir -p ../include ../lib
         cp ./build/hailort/libhailort/src/libhailort.so.$HAILORT_VERSION ../lib/
         cp hailort/libhailort/include/hailo/*.h ../include/
         ;;
     windows-x86_64)
         cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release
         cmake --build build --config release --target libhailort
+        mkdir -p ../include ../lib
 
         ;;
     *)
--- hailort/src/main/java/org/bytedeco/hailort/presets/hailort.java
+++ hailort/src/main/java/org/bytedeco/hailort/presets/hailort.java
@@ -6,8 +6,7 @@ import org.bytedeco.javacpp.tools.*;
 
 @Properties(
         value = {
-                @Platform(include = "<zlib.h>", link = "z@.1#"),
-                @Platform(value = "windows", link = "zlib#")
+                @Platform(include = "hailort.h"),
         },
         target = "org.bytedeco.hailort",
         global = "org.bytedeco.hailort.global.zlib"
@@ -20,18 +19,31 @@ public class hailort implements InfoMapper {
 
     @Override
     public void map(InfoMap infoMap) {
-        infoMap.put(new Info("ZEXTERN", "ZEXPORT", "z_const", "zlib_version").cppTypes().annotations())
-                .put(new Info("FAR").cppText("#define FAR"))
-                .put(new Info("OF").cppText("#define OF(args) args"))
-                .put(new Info("Z_ARG").cppText("#define Z_ARG(args) args"))
-                .put(new Info("Byte", "Bytef", "charf").cast().valueTypes("byte").pointerTypes("BytePointer"))
-                .put(new Info("uInt", "uIntf").cast().valueTypes("int").pointerTypes("IntPointer"))
-                .put(new Info("uLong", "uLongf", "z_crc_t", "z_off_t", "z_size_t").cast().valueTypes("long").pointerTypes("CLongPointer"))
-                .put(new Info("z_off64_t").cast().valueTypes("long").pointerTypes("LongPointer"))
-                .put(new Info("voidp", "voidpc", "voidpf").valueTypes("Pointer"))
-                .put(new Info("gzFile_s").pointerTypes("gzFile"))
-                .put(new Info("gzFile").valueTypes("gzFile"))
-                .put(new Info("Z_LARGE64", "!defined(ZLIB_INTERNAL) && defined(Z_WANT64)").define(false))
-                .put(new Info("inflateGetDictionary", "gzopen_w", "gzvprintf").skip());
+        infoMap.put(new Info("HAILO_MAX_ENUM", "HAILO_INFINITE", "HAILO_DEFAULT_ETH_SCAN_TIMEOUT_MS",
+                        "HAILO_DEFAULT_ETH_CONTROL_PORT", "HAILO_DEFAULT_ETH_DEVICE_PORT",
+                        "HAILO_DEFAULT_ETH_MAX_PAYLOAD_SIZE", "HAILO_DEFAULT_ETH_MAX_NUMBER_OF_RETRIES",
+                        "HAILO_ETH_ADDRESS_ANY", "HAILO_ETH_PORT_ANY", "HAILO_MAX_NAME_SIZE",
+                        "HAILO_MAX_STREAM_NAME_SIZE", "HAILO_MAX_BOARD_NAME_LENGTH", "HAILO_MAX_DEVICE_ID_LENGTH",
+                        "HAILO_MAX_SERIAL_NUMBER_LENGTH", "HAILO_MAX_PART_NUMBER_LENGTH",
+                        "HAILO_MAX_PRODUCT_NAME_LENGTH", "HAILO_DEFAULT_INIT_SAMPLING_PERIOD_US",
+                        "HAILO_DEFAULT_INIT_AVERAGING_FACTOR", "HAILO_DEFAULT_BUFFERS_THRESHOLD",
+                        "HAILO_DEFAULT_MAX_ETHERNET_BANDWIDTH_BYTES_PER_SEC", "HAILO_MAX_STREAMS_COUNT",
+                        "HAILO_DEFAULT_BATCH_SIZE", "HAILO_MAX_NETWORK_GROUPS", "HAILO_MAX_NETWORK_GROUP_NAME_SIZE",
+                        "HAILO_MAX_NETWORK_NAME_SIZE", "HAILO_MAX_NETWORKS_IN_NETWORK_GROUP", "HAILO_PCIE_ANY_DOMAIN",
+                        "HAILO_DEFAULT_VSTREAM_QUEUE_SIZE", "HAILO_DEFAULT_VSTREAM_TIMEOUT_MS",
+                        "HAILO_DEFAULT_ASYNC_INFER_TIMEOUT_MS", "HAILO_DEFAULT_ASYNC_INFER_QUEUE_SIZE",
+                        "HAILO_DEFAULT_DEVICE_COUNT", "HAILO_SOC_ID_LENGTH", "HAILO_ETH_MAC_LENGTH",
+                        "HAILO_UNIT_LEVEL_TRACKING_BYTES_LENGTH", "HAILO_SOC_PM_VALUES_BYTES_LENGTH",
+                        "HAILO_MAX_TEMPERATURE_THROTTLING_LEVELS_NUMBER", "HAILO_UNIQUE_VDEVICE_GROUP_ID",
+                        "HAILO_DEFAULT_VDEVICE_GROUP_ID", "HAILO_SCHEDULER_PRIORITY_NORMAL",
+                        "HAILO_SCHEDULER_PRIORITY_MAX", "HAILO_SCHEDULER_PRIORITY_MIN", "MAX_NUMBER_OF_PLANES",
+                        "NUMBER_OF_PLANES_NV12_NV21", "NUMBER_OF_PLANES_I420").cppTypes().annotations())
+
+
+                .put(new Info("HAILO_STATUS_VARIABLES").translate(false)) // Enums are defined via macros, handle separately if necessary
+
+                .put(new Info("float32_t", "float64_t", "nms_bbox_counter_t").cast().pointerTypes("FloatPointer", "DoublePointer", "IntPointer"))
+                .put(new Info("hailo_version_t").translate()) // This will require a manual translation, mapping struct fields
+                .put(new Info("hailo_status").cast().valueTypes("int").pointerTypes("IntPointer"));
     }
 }
