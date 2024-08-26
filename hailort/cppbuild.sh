453c2aff795d88a98c585321dce3449fe5d37332
barry <barry@ubiquitech.co.za>
2024-08-26 11:53:36 +0200

    hailort support

---
+++ hailort/cppbuild.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# This file is meant to be included by the parent cppbuild.sh script
+if [[ -z "$PLATFORM" ]]; then
+    pushd ..
+    bash cppbuild.sh "$@" zlib
+    popd
+    exit
+fi
+
+HAILORT_VERSION=4.18.0
+download https://github.com/hailo-ai/hailort/archive/refs/tags/v$HAILORT_VERSION.tar.gz hailort-v$HAILORT_VERSION.tar.gz
+mkdir -p $PLATFORM
+cd $PLATFORM
+tar -xzvf ../hailort-$HAILORT_VERSION.tar.gz
+cd hailort-$HAILORT_VERSION
+
+case $PLATFORM in
+    android-arm64)
+        cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release
+        cmake --build build --config release --target libhailort
+        cp ./build/hailort/libhailort/src/libhailort.so.$HAILORT_VERSION ../lib/
+        cp hailort/libhailort/include/hailo/*.h ../include/
+        ;;
+    android-x86_64)
+        cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release
+        cmake --build build --config release --target libhailort
+        ;;
+    linux-x86_64)
+        cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release
+        cmake --build build --config release --target libhailort
+        cp ./build/hailort/libhailort/src/libhailort.so.$HAILORT_VERSION ../lib/
+        cp hailort/libhailort/include/hailo/*.h ../include/
+        ;;
+    linux-arm64)
+        cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release
+        cmake --build build --config release --target libhailort
+        cp ./build/hailort/libhailort/src/libhailort.so.$HAILORT_VERSION ../lib/
+        cp hailort/libhailort/include/hailo/*.h ../include/
+        ;;
+    windows-x86_64)
+        cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release
+        cmake --build build --config release --target libhailort
+
+        ;;
+    *)
+        echo "Error: Platform \"$PLATFORM\" is not supported"
+        ;;
+esac
+
+cd ../..
