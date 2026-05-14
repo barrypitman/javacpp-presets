292f23d8e0b047ffb970b5e6296962400b695d42
Barry Pitman <barrypitman@users.noreply.github.com>
2026-05-14 10:25:39 +0200

    opencv: add e-con DirectShow videoio patch

--- opencv/cppbuild.sh
+++ opencv/cppbuild.sh
@@ -78,6 +78,7 @@ patch -Np1 < ../../../opencv_contrib-cuda13.patch
 cd ../opencv-$OPENCV_VERSION
 patch -Np1 < ../../../opencv.patch
 patch -Np1 < ../../../opencv-econ-videoio.patch
+patch -Np1 < ../../../opencv-econ-videoio-dshow.patch
 #patch -Np1 < ../../../opencv-cuda13.patch
 #patch -Np1 < ../../../opencv-cudnn8.patch
 patch -Np1 < ../../../opencv-linux-ppc64le.patch
