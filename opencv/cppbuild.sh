32603d5572560198e56349106d7394a2a370bd75
Barry Pitman <barrypitman@users.noreply.github.com>
2026-05-14 14:27:20 +0200

    opencv: support Java String output wrappers

--- opencv/cppbuild.sh
+++ opencv/cppbuild.sh
@@ -77,6 +77,7 @@ patch -Np1 < ../../../opencv_contrib-cuda13.patch
 
 cd ../opencv-$OPENCV_VERSION
 patch -Np1 < ../../../opencv.patch
+patch -Np1 < ../../../opencv-java-out-string.patch
 patch -Np1 < ../../../opencv-econ-videoio.patch
 patch -Np1 < ../../../opencv-econ-videoio-dshow.patch
 #patch -Np1 < ../../../opencv-cuda13.patch
